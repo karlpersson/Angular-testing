f98d576dbd4852c85c2cf00fc9d249ded921f2bb
Karl Persson <karl.persson@essvision.se>
2018-01-17 12:22:17 +0100

    test specs

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -12,4 +12,13 @@ describe('Service: Auth',() => {
         localStorage.removeItem('token');
     });
 
+    it('should return true from isAuthenticated when there is a token', () => {
+        localStorage.setItem('token', '1234');
+        expect(service.isAuthenticated()).toBeTruthy();
+    });
+
+    it('should return false from isAuthenticated when there is no token', () => {
+        expect(service.isAuthenticated()).toBeFalsy();
+    });
+
 })
